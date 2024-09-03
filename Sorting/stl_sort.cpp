26518181020a718a76de8e0a9edf5074166543c1
Luxar-cmd <eduardo.moraless@usm.cl>
2024-09-02 22:29:28 -0400

    El sorting de la standard library es agregado

--- Sorting/stl_sort.cpp
+++ Sorting/stl_sort.cpp
@@ -3,8 +3,16 @@
 using namespace std;
 
 int main() {
-    
+    vector<int> arr = {1,2,8,9,6,7,3,4,2,0};
+    int n = arr.size();
 
+    sort(arr.begin(), arr.begin() + n);
+
+    cout << "Array after sorting using default sort is : \n";
+    for ( int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
 
