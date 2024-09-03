5d36be3681ca0f7de2e5ef02873f4c394a4c1afb
Luxar-cmd <eduardo.moraless@usm.cl>
2024-09-03 15:49:48 -0400

    Implementación de la multiplicación clásica de matrices.

---
+++ Multiplication/matrix_mult.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+
+#define matrix vector<vector<int>>
+
+
+/*
+Suposiciónes del programa:
+- Las matrices ingresadas tienen siempre una estructura correcta, significa que las rectangulares están en el orden correcto.
+*/
+
+
+
+
+
+
+matrix matrix_mult(matrix& m1, matrix& m2){
+    int FR, FC, SR, SC;
+    FR = m1.size(); //Cantidad de filas primera matriz
+    FC = m1[0].size(); //Cantidad de columnas primera matriz
+    SR = m2.size(); //Cantidad de filas segunda matriz
+    SC = m2[0].size(); //Cantidad de columnas segunda matriz
+    matrix result(FR,vector<int>(SC,0)); //La matriz resultante tiene FR * SC casillas.
+
+    for ( int i = 0; i < FR; ++i) {
+        for ( int j = 0; j < SC; ++j) {
+            for ( int k = 0; k < FC; ++k) {
+                result[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+    return result;
+}
+
+//Función que lee el contenido del dataset y los guarda en una matriz de vectores.
+matrix read_matrixs(ifstream& data) {    
+    string line; 
+    int n, m, num;
+    
+
+    
+
+    if(!(data >> n >> m)) {
+        return matrix(); //Retorna una matriz vacía si no hay más matrices para leer
+    }
+    matrix trix(n, vector<int>(m,0));
+    for ( int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            data >> num;
+            trix[i][j] = num;
+            
+        }
+    }
+    return trix;
+}
+
+//Esta función hace print de las matrices, de cualquier tamaño
+void print_matrix(const matrix& mat) {
+    if (mat.empty()) {
+        cout << "Matrix is empty or could not be read!" << endl;
+        return;
+    }
+
+    for (const auto& row : mat) {
+        for (const auto& val : row) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+    ifstream data("data.txt");
+
+    if ( !data.is_open()) {
+        cerr << "Error opening the file" << endl;
+        return 1;
+    }
+
+    
+    while (true)
+    {
+        matrix m1 = read_matrixs(data);
+        if (m1.empty()) break;
+
+        matrix m2 = read_matrixs(data);
+        if (m2.empty()) break;
+        
+        auto start = chrono::high_resolution_clock::now();
+
+        matrix result = matrix_mult(m1,m2);
+
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::microseconds>(end-start);
+        
+        cout << duration.count() << " ms" << endl;
+
+        cout << "Resultado: " << endl;
+        print_matrix(result);
+    }
+
+    
+    data.close();
+
+    return 0;
+}
